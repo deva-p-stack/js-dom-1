c4c0ecf796cceb341ad7ab885f1560082e0b2aae
deva-p-stack <devap677@gmail.com>
2026-01-13 10:48:04 +0530

    FIX: layout isses;

--- src/main.js
+++ src/main.js
@@ -14,19 +14,24 @@ let containersection = document.querySelector('.containersection');
     greetbtn.classList.remove("-mt-5");
     return ;
   }
+  else if(/\d/.test(inputname.value)){
+    errorname.classList.remove("invisible");
+    greetbtn.classList.remove("-mt-5");
+    return ;
+  }
   else{
     errorname.classList.add("invisible");
-    errorname.classList.add("-mt-5");
+    errorname.classList.add("mt-2");
   }
 
 
     let namevalue = inputname.value;
     let newdiv = document.createElement("div");
-    newdiv.className="flex flex-col w-full max-w-sm bg-green-100 m-5 justify-center items-center rounded-2xl h-[30vh]";
+    newdiv.className="flex flex-col w-full max-w-md bg-green-100 m-5 justify-center items-center rounded-2xl ";
     
     let content = document.createElement("h2");
-    content.className="text-xl font-semibold text-center p-3";
-    content.textContent = `Hello ${namevalue}, Welcome to CyberDude!`;
+    content.className="text-lg font-semibold text-center p-3";
+    content.textContent = `Hello ${namevalue.charAt(0).toUpperCase()}${namevalue.slice(1)}, Welcome to CyberDude 💙✨!`;
 
     newdiv.append(content);
     containersection.append(newdiv);
